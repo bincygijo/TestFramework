b9e0e2b54786c0b45e8d7cd23be93168d25c206e
bincy <bincy@xmdevelopments.com>
2019-09-13 11:29:49 +1200

    first

---
+++ FrameworkPageObjectModel/Pages/AdminDashboard.cs
@@ -0,0 +1,24 @@
+﻿using FrameworkPageObjectModel.Global;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static FrameworkPageObjectModel.Global.CommonMethods;
+
+namespace FrameworkPageObjectModel.Pages
+{
+   public class AdminDashboard
+    {
+        public void ValidateAdmin()
+        {
+            //Populate in collection
+            CommonMethods.ExcelLib.PopulateInCollection(Base.ExcelPath, "admindashboard");
+
+            //Clicking on Owner dropdown
+            GlobalDefinitions.ActionButton(GlobalDefinitions.driver, ExcelLib.ReadData(2, "Locator"), ExcelLib.ReadData(2, "LocatorValue"));
+            Thread.Sleep(2000);
+        }
+    }
+}
---
+++ FrameworkPageObjectModel/Test/MainClass.cs
@@ -0,0 +1,38 @@
+﻿using FrameworkPageObjectModel.Global;
+using FrameworkPageObjectModel.Pages;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkPageObjectModel.Test
+{
+  public class MainClass
+    {
+        [TestFixture]
+        [Category("Sprint_1")]
+        class Sprint_1_Administration : Base
+        {
+
+
+            [Test]
+            public void ValidateAdminDashboard()
+            {
+                
+                // starting test
+                test = extent.StartTest("Validate Admin Dashboard test case starts");
+                AdminDashboard objAdmindashboard = new AdminDashboard();
+                objAdmindashboard.ValidateAdmin();
+               
+
+            }
+
+
+
+
+
+        }
+    }
+}
